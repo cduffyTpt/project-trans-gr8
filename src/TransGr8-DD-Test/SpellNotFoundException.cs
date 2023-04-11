9228f32cb3e8e4db0553a6a2503aa13c2ff890a3
gryoussef <youssef.jirari@gmail.com>
2023-04-11 02:27:27 +0100

    Move sppels retrival logic to SpellRepository

---
+++ src/TransGr8-DD-Test/SpellNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace TransGr8_DD_Test
+{
+    internal class SpellNotFoundException : Exception
+    {
+        public SpellNotFoundException()
+        {
+        }
+
+        public SpellNotFoundException(string message) : base(message)
+        {
+        }
+
+        public SpellNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected SpellNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/TransGr8-DD-Test/SpellRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Immutable;
+
+namespace TransGr8_DD_Test
+{
+    internal interface ISpellRepository
+    {
+        Spell GetSpell(string spellName);
+    }
+    internal class SpellRepository : ISpellRepository
+    {
+		private readonly ImmutableArray<Spell> _spellList;
+        public SpellRepository(List<Spell> spellList)
+        {
+            _spellList = spellList.ToImmutableArray();
+        }
+        public Spell GetSpell(string spellName)
+        {
+            return _spellList.FirstOrDefault(spell => String.Equals(spell.Name, spellName)) ?? throw new SpellNotFoundException($"{spellName} Spell is not found");
+        }
+    }
+}
