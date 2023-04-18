82906c05b550ae302f9bd4bf59b6c0a3d45ca7c5
Taha Lamti <tlamti@masaratapp.com>
2023-04-18 04:43:10 +0000

    Serilog configuration

---
+++ src/TransGr8-DD-Test/Services/LoggerService.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransGr8_DD_Test.Services
+{
+    internal class LoggerService
+    {
+        public LoggerService()
+        {
+            //Configure the Logger
+            Log.Logger = new LoggerConfiguration().WriteTo.File("Logs.log").CreateLogger();
+        }
+    }
+}
